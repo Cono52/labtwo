5d2e53241b2dabe3e14e7e91567033e11bc64549
Cono52 <oflanac52@gmail.com>
2016-10-25 20:48:30 +0100

    Added script to install node and start server.

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#install Node.js
+if [[ $(node -v) = *v* ]]; then
+	echo "node is installed"
+else
+	echo " Couldn't find node installation...INSTALLING"
+	curl https://deb.nodesource.com/setup_6.x | sudo -E bash -
+	sudo apt-get install -y nodejs
+fi
+printf "\n\n"
+
+#run server
+node server
