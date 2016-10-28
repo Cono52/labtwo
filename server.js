2fcc140125324b3e71a410d7378e7eedb14c14b1
Cono52 <oflanac52@gmail.com>
2016-10-28 17:45:51 +0100

    Added function to decode symbols in url
    Also added a catch in the case of malformed URI
    which I found was possible given particular
    characters in the request. Fixed up indents
    and added comments.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const port = 3000
 const  os = require('os');
 
-
+/*Get IP address*/
 let interfaces = os.networkInterfaces();
 let addresses = [];
 for (let k in interfaces) {
@@ -16,21 +16,31 @@ for (let k in interfaces) {
 
 console.log(addresses);
 
+/*Function for decoding url strings sent in requests*/
+function decode(string) {
+	try{
+		return decodeURIComponent(string.replace(/\+/g,  " "));
+	} catch (ex) {
+		return "/Error occured, malformed url"
+	}
+}
+
 const requestHandler = (request, response) => {
-	console.log(request.url)
+	console.log(decode(request.url))
+	let decoded = decode(request.url)
 	if(request.url.includes("KILL_SERVICE")){
 		response.end("Service Shutdown..\n");
 		process.exit();
 	}
 	else if(request.url.includes("HELO"))
 	{
-		response.end(request.url.slice(1,request.url.length)+"\n"
-			     +"IP:"+addresses+"\n"
-			     +"PORT:"+port+"\n"
-			     +"StudentID:13323109\n")
+		response.end(decoded.slice(1,request.url.length)+"\n"
+					+"IP:"+addresses+"\n"
+					+"PORT:"+port+"\n"
+					+"StudentID:13323109\n")
 	}
 	else {
-		response.end("Please type 'HELO \\n' to get a valid response.\n")
+		response.end('Please type "HELO text\\n" to get a valid response.\n')
 	}
 }
 
