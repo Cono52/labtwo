7c3996aadd0774d4c7032318018ca6a13c8d044c
Cono52 <oflanac52@gmail.com>
2016-10-31 16:54:38 +0000

    Changed over to using net instead of http
    The submission server tests weren't working
    using the http abstraction as it's just for
    non-persistent url requests.
    
    net is the underlying socket implementation
    of the http package, both of which are built
    in to node and can be used separately.
    By using net instead of http the submission
    test server managed to work as I could respond
    raw data vs http requests.
    
    Also removed all the semi-colons.

--- server.js
+++ server.js
@@ -1,51 +1,44 @@
-const http = require('http')
+const net = require('net')
 const port = 3000
-const  os = require('os');
+const  os = require('os')
 
 /*Get IP address*/
-let interfaces = os.networkInterfaces();
-let addresses = [];
+let interfaces = os.networkInterfaces()
+let addresses = []
 for (let k in interfaces) {
 	for (let k2 in interfaces[k]) {
-		let address = interfaces[k][k2];
+		let address = interfaces[k][k2]
 		if (address.family === 'IPv4' && !address.internal) {
-			addresses.push(address.address);
+			addresses.push(address.address)
 		}
 	}
 }
 
-console.log(addresses);
+console.log(addresses)
 
-/*Function for decoding url strings sent in requests*/
-function decode(string) {
-	try{
-		return decodeURIComponent(string.replace(/\+/g,  " "));
-	} catch (ex) {
-		return "/Error occured, malformed url"
-	}
-}
+const requestHandler = (sock) => {
+	console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort)
 
-const requestHandler = (request, response) => {
-	console.log(decode(request.url))
-	let decoded = decode(request.url)
-	if(request.url.includes("KILL_SERVICE")){
-		response.end("Service Shutdown..\n");
-		process.exit();
-	}
-	else if(request.url.includes("HELO"))
-	{
-		response.end(decoded.slice(1,request.url.length)+"\n"
-					+"IP:"+addresses+"\n"
-					+"PORT:"+port+"\n"
-					+"StudentID:13323109\n")
-	}
-	else {
-		response.end('Please type "HELO text\\n" to get a valid response.\n')
-	}
+	sock.on('data', function(data) {
+		if(data.includes("HELO")){
+			sock.write(data
+				+"IP:"+addresses+"\n"
+				+"Port:"+port+"\n"
+				+"StudentID:13323109\n")
+
+		}
+		else if(data.includes("KILL_SERVICE")){
+			sock.destroy();
+		}
+	})
+
+	sock.on('close', function(data) {
+		console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort)
+	})
 }
 
 
-const server = http.createServer(requestHandler)
+const server = net.createServer(requestHandler)
 
 server.listen(port, (err) => {
 	if (err) {
